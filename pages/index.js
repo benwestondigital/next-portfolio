856b0de931fbd929da3b7aba97762a55cad60905
Ben <benwestondigital@gmail.com>
2022-03-12 19:22:03 +0000

    add homepage to index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import HomePage from './homePage';
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Ben Weston Portfolio</title>
-        <meta name="description" content="Ben's portfolio"/>
+        <meta name='description' content="Ben's portfolio" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-    <Navbar/>
+      <Navbar />
+      <HomePage />
     </div>
-  )
+  );
 }
