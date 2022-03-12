2782275f1b945a1c3064e219b31c693e63c60e7e
Ben <benwestondigital@gmail.com>
2022-03-12 23:39:20 +0000

    remove tech props add icon hover colour change

--- components/TechSection.jsx
+++ components/TechSection.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-
-const TechSection = ({techType, tech}) => {
+const TechSection = ({techType}) => {
   return (
     <div className='flex justify-items-start items-center mx-40 gap-10 mt-20'>
       <div className='flex flex-col justify-center items-center'>
         <h2 className='text-xl text-indigo-600 font-semibold'>{techType[0].type}</h2>
         <div className='flex justify-center items-center cursor-pointer'>
-          {techType.map((item, index) => {
-            const Icon = tech[index].icon;
+          {techType.map((item) => {
+            const Icon = item.icon;
             return (
               <div
                 key={item.techName}
                 className='flex flex-col justify-center items-center mx-2'
               >
                 <Icon
-                  className={`w-10 h-10 bg-${item.colour}-400 text-gray-100 m-3 p-1 shadow-xl rounded-md`}
+                  className={`w-10 h-10 bg-gray-600 hover:bg-gray-800 text-gray-100 m-3 p-1 shadow-xl rounded-md`}
                 />
                 <p className='md:text-base text-gray-400 md:text-center sm:text-left sm:text-sm'>
                   {item.techName}
