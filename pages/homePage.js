d5f3beb3b4172ae5c5fc75939f12f1080d287a02
Ben <benwestondigital@gmail.com>
2022-03-12 19:29:55 +0000

    add text and anchor tag

--- pages/homePage.js
+++ pages/homePage.js
@@ -19,6 +19,17 @@ const HomePage = () => {
           <h1 className='font-bold text-7xl text-left mb-5'>
             Hello, I am <span className='text-indigo-900'>Ben</span>
           </h1>
+          <p className='text-left font-normal font mb-5 flex-wrap'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Perspiciatis, repellendus est incidunt, ea explicabo dolores amet
+            ducimus debitis numquam temporibus eligendi sed quo necessitatibus?
+            Provident animi consequuntur debitis at? Recusandae!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Perspiciatis, repellendus est incidunt, ea explicabo dolores amet
+            ducimus debitis numquam temporibus eligendi sed quo necessitatibus?
+            Provident animi consequuntur debitis at? Recusandae!
+          </p>
+          <a href="#" className='font-semibold text-white md:mt-10 mt-5 pt-5 bg-indigo-900 rounded-md w-60 h-16 text-lg hover:bg-black'>See My Portfolio</a>
         </div>
       </div>
     </div>
