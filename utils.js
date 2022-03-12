b45e0b0287cc2f801531c7296ae68815c6f6f88c
Ben <benwestondigital@gmail.com>
2022-03-12 21:23:27 +0000

    tech array obj

---
+++ utils.js
@@ -0,0 +1,41 @@
+const ztech = [{ techName: '', type: '', icon: '', colour: '' }];
+
+const tech = [
+  { techName: 'JavaScript', type: 'Front End', icon: 'DiJsBadge', colour: '' },
+  {
+    techName: 'TypeScript',
+    type: 'Front End',
+    icon: 'SiTypescript',
+    colour: '',
+  },
+  { techName: 'React', type: 'Front End', icon: 'DiReact', colour: '' },
+  { techName: 'Svelte', type: 'Front End', icon: 'SiSvelte', colour: '' },
+  { techName: 'HTML', type: 'Front End', icon: 'AiFillHtml5', colour: '' },
+  { techName: 'CSS', type: 'Front End', icon: 'DiCss3', colour: '' },
+  {
+    techName: 'PostgreSQL',
+    type: 'Back End',
+    icon: 'DiPostgresql',
+    colour: '',
+  },
+  { techName: 'Express', type: 'Back End', icon: 'SiExpress', colour: '' },
+  { techName: 'Node.js', type: 'Back End', icon: 'SiNodedotjs', colour: '' },
+  { techName: 'MongoDB', type: 'Back End', icon: 'DiMongodb', colour: '' },
+  { techName: 'Jest', type: 'Other', icon: 'SiJest', colour: '' },
+  { techName: 'GitHub', type: 'Other', icon: 'DiGithubBadge', colour: '' },
+  { techName: 'Git', type: 'Other', icon: 'FaGit', colour: '' },
+  {
+    techName: 'Google Analytics',
+    type: 'Other',
+    icon: 'DiGoogleAnalytics',
+    colour: '',
+  },
+  {
+    techName: 'Google Tag Manager',
+    type: 'Other',
+    icon: 'SiGoogletagmanager',
+    colour: '',
+  },
+  { techName: 'Heroku', type: 'Other', icon: 'DiHeroku', colour: '' },
+  { techName: 'Linux', type: 'Other', icon: 'DiLinux', colour: '' },
+];
