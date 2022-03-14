b4a58cea158b68b3d253cdb79ab1f769e583dc75
Ben <benwestondigital@gmail.com>
2022-03-14 21:37:34 +0000

    add margin to layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className='content'>
+    <div className='mx-3 sm:mx-4 md:mx-5 lg:mx-6 xl:mx-10'>
       <Navbar />
       {children}
       <Footer />
