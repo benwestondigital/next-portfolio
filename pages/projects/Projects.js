19486b059b9425d3d832a5886b616f9446a40aa0
Ben <benwestondigital@gmail.com>
2022-03-14 16:03:22 +0000

    add project obj and image to project file

--- pages/projects/Projects.js
+++ pages/projects/Projects.js
@@ -1,8 +1,18 @@
 import { Element } from 'react-scroll';
 import SingleProject from '../../components/SingleProject';
-import { projects } from '../../utils';
+import whereto from '../../public/images/whereto.png'
+import redditfront from '../../public/images/redditfront.png'
+import redditback from '../../public/images/redditback.png'
+import Image from 'next/image';
+
 
 const Projects = () => {
+  const projects = [
+    { projName: 'WhereTo - Climate Travel App', image: whereto },
+    { projName: 'Front-End Reddit', image: redditfront },
+    { projName: 'Back-End Reddit', image: redditback },
+  ];
+
   return (
     <Element id='projects' name='projects'>
       <div className='w-full my-20 h-auto flex flex-col justify-center items-center'>
@@ -11,10 +21,8 @@ const Projects = () => {
           Web Development Projects
         </h2>
         <div className='md:flex sm:flex-col md:flex-row justify-center items-center gap-10 mt-10'>
-          {projects.map((item) => {
-          return (
-          <SingleProject key={item.projName} projName={item.projName} />
-          )
+          {projects.map(project => {
+            return <SingleProject key={project.projName} project={project} />;
           })}
         </div>
       </div>
