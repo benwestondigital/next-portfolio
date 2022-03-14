08e0c780cebf473a4c9c6cbb5f8fb7c7aefa5289
Ben <benwestondigital@gmail.com>
2022-03-14 16:02:18 +0000

    add images to project section

--- components/SingleProject.jsx
+++ components/SingleProject.jsx
@@ -1,21 +1,23 @@
 import Image from 'next/image';
-import heroImage from '../public/images/heroimage.jpeg';
 
-const SingleProject = ({projName}) => {
+const SingleProject = ({ project }) => {
   return (
-      <div className='flex flex-col items-center'>
-    <div className='rounded w-80 h-60 shadow-xl relative'>
-    <Image
-      src={heroImage}
-      alt='project'
-      layout='fill'
-      objectFit='cover'
-      className='p-2 rounded cursor-pointer'
-    />
-  </div>
-    <h2 className='text-xl text-indigo-600 font-semibold'>{projName}</h2>
+    <div className='flex flex-col items-center'>
+      <div className='rounded w-80 h-80 shadow-xl relative'>
+        <Image
+          src={project.image}
+          alt='project'
+          layout='fill'
+          objectFit='cover'
+          className='p-2 rounded cursor-pointer'
+          objectPosition='top'
+        />
+      </div>
+      <h2 className='text-xl text-indigo-600 font-semibold'>
+        {project.projName}
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default SingleProject
+export default SingleProject;
