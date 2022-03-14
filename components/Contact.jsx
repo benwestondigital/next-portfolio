63eef07c980c9bddaeda62984ffca1284384c037
Ben <benwestondigital@gmail.com>
2022-03-14 22:14:22 +0000

    add todo and increase margin

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,11 +4,16 @@ const Contact = () => {
   return (
     <Element id='contact' name='contact'>
       <div>
-        <div className='w-full my-20 h-auto flex flex-col justify-center items-center'>
+        <div className='w-full my-40 h-auto flex flex-col justify-center items-center'>
           <h2 className='text-indigo-900 text-6xl font-bold text-center'>
             Contact Me
           </h2>
-          <form className='flex justify-center text-center md:w-1/2 w-full my-5'>
+          <p className='text-center md:text-left font-normal font mx-2 mt-6 flex-wrap'>
+              Interested in talking about a job opportunity? Please contact me
+              on social media or email me here:
+            </p>
+          {/* TODO: build out this form to include name, email, bigger text area */}
+          <form className='flex justify-center text-center md:w-1/2 w-full my-3'>
             <input
               type='text'
               name='email-address'
