923e5550ea12b1da71f94f199e73f138b94c7927
Ben <benwestondigital@gmail.com>
2022-03-14 21:22:43 +0000

    decrease margin in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className='w-full h-16 bg-gray-50 flex justify-center items-center'>
       <h3 className='text-xl text-blue-900 mr-2'>Made by</h3>
-      <h3 className='text-xl text-blue-900 ml-2'>
+      <h3 className='text-xl -ml-0.5 text-blue-900'>
         <a
           href='#'
           className='cursor-pointer font-semibold text-indigo-900 hover:text-black'
