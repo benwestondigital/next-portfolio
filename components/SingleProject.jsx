5b799422f0fd942ba6d5ea8621898d3e4c33c765
Ben <benwestondigital@gmail.com>
2022-03-14 19:35:58 +0000

    add arrow to link

--- components/SingleProject.jsx
+++ components/SingleProject.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const SingleProject = ({ project }) => {
   return (
@@ -16,8 +17,14 @@ const SingleProject = ({ project }) => {
       <h2 className='text-xl m-2 text-indigo-600 font-semibold'>
         {project.projName}
       </h2>
-      <p className='text-left font-normal font mb-5 flex-wrap'>{project.description}</p>
-      <a href={`/${project.slug}`} className='uppercase text-left font-normal font mb-5 flex-wrap'>See Project</a>
+      <p className='text-left font-normal font mb-5 flex-wrap'>
+        {project.description}
+      </p>
+      <Link
+        href={`/projects/${project.slug}`}
+      >
+        <a className='uppercase text-left font-normal font mb-5 flex-wrap'>See Project &gt</a>
+      </Link>
     </div>
   );
 };
