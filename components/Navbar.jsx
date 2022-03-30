6ab3bf4cf100a9c6730c376065dff449d0368a6e
Ben <benwestondigital@gmail.com>
2022-03-30 12:49:29 +0100

    refactor mobile links to map

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -135,50 +135,22 @@ const Navbar = () => {
         >
           <div className='md:hidden' id='mobile-menu'>
             <div className='bg-white mx-4 mr-20 pt-4 pb-4 space-y-1'>
-              <Link
-                href='/about'
-                activeClass='about'
-                to='about'
-                smooth={true}
-                offset={50}
-                duration={500}
-                className='cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              >
-                About
-              </Link>
-              <Link
-                href='/projects'
-                activeClass='projects'
-                to='projects'
-                smooth={true}
-                offset={50}
-                duration={500}
-                className='cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              >
-                Projects
-              </Link>
-              <Link
-                href='/tech'
-                activeClass='tech'
-                to='tech'
-                smooth={true}
-                offset={50}
-                duration={500}
-                className='cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              >
-                My Skills
-              </Link>
-              <Link
-                href='/contact'
-                activeClass='contact'
-                to='contact'
-                smooth={true}
-                offset={50}
-                duration={500}
-                className='cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              >
-                Contact
-              </Link>
+              {navLinks.map(link => {
+                return (
+                  <Link
+                    key={link.path}
+                    href={`/${link.path}`}
+                    activeClass={link.path}
+                    to={link.path}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    className='cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+                  >
+                    {link.name}
+                  </Link>
+                );
+              })}
             </div>
           </div>
         </Transition>
