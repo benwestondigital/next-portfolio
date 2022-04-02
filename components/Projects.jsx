d152ded9711d3de52ab98ebe5f8a383a45e43413
Ben <benwestondigital@gmail.com>
2022-04-02 12:30:10 +0100

    change from object to using getstaticprops

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,27 +1,17 @@
 import { Element } from 'react-scroll';
 import SingleProject from './SingleProject';
-import whereto from '../public/images/whereto.png'
-import redditfront from '../public/images/redditfront.png'
-import redditback from '../public/images/redditback.png'
 
-
-const Projects = () => {
-  const projects = [
-    { slug: 'whereto', projName: 'WhereTo - Climate Travel App', image: whereto, description: 'Miss the weather in your favourite holiday destination? Let WhereTo help you decide on your next vacation spot. Uses Svelte / Svelte Kit.' },
-    { slug: 'reddit-front', projName: 'Front-End Reddit', image: redditfront, description: 'Developed a Reddit clone front-end with React.' },
-    { slug: 'reddit-back', projName: 'Back-End Reddit', image: redditback, description: 'The back-end functionality for my Reddit clone built using Express / PostgreSQL.' },
-  ];
-
+const Projects = ({projects}) => {
   return (
     <Element id='projects' name='projects'>
       <div className='flex flex-col items-center justify-center w-full h-auto my-32'>
-        <h2 className='text-6xl font-bold text-indigo-900'>
-          Projects
-        </h2>
-        <p className='mt-5 mb-5 font-normal text-center'>Here are some of the projects I&apos;ve been working on lately:</p>
+        <h2 className='text-6xl font-bold text-indigo-900'>Projects</h2>
+        <p className='mt-5 mb-5 font-normal text-center'>
+          Here are some of the projects I&apos;ve been working on lately:
+        </p>
         <div className='items-center justify-center gap-10 mt-10 md:flex flex-wrap sm:flex-col md:flex-row md:items-start md:gap-10 sm:pb-10'>
           {projects.map(project => {
-            return <SingleProject key={project.projName} project={project} />;
+            return <SingleProject key={project.slug} project={project} />;
           })}
         </div>
       </div>
--- components/SingleProject.jsx
+++ components/SingleProject.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const SingleProject = ({ project }) => {
+const SingleProject = ({ project: { slug, frontmatter } }) => {
   return (
     <div className='flex flex-col w-96 items-center justify-start'>
       <div className='rounded w-80 h-80 shadow-xl relative'>
         <Image
-          src={project.image}
-          alt={project.projName}
+          src={`/${frontmatter.socialImage}`}
+          alt={frontmatter.title}
           layout='fill'
           objectFit='cover'
           className='p-2 rounded cursor-pointer'
@@ -15,15 +15,15 @@ const SingleProject = ({ project }) => {
         />
       </div>
       <h2 className='text-xl m-2 mt-5 text-indigo-600 font-semibold'>
-        {project.projName}
+        {frontmatter.title}
       </h2>
       <p className='text-center font-normal mx-2 mb-5 flex-wrap'>
-        {project.description}
+        {frontmatter.description}
       </p>
-      <Link
-        href={`/projects/${project.slug}`}
-      >
-        <a className='uppercase text-center font-normal font mb-5 flex-wrap'>See Project Details &gt;</a>
+      <Link href={`/projects/${slug}`}>
+        <a className='uppercase text-center font-normal font mb-5 flex-wrap'>
+          See Project Details &gt;
+        </a>
       </Link>
     </div>
   );
