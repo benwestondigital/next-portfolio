5d52868083fe739ce5ee26326922c356221c9b46
Ben <benwestondigital@gmail.com>
2022-04-03 14:03:36 +0100

    set whole section to be link

--- components/SingleProject.jsx
+++ components/SingleProject.jsx
@@ -3,29 +3,30 @@ import Link from 'next/link';
 
 const SingleProject = ({ project: { slug, frontmatter } }) => {
   return (
-    <div className='flex flex-col w-96 items-center justify-start'>
-      <div className='rounded w-80 h-80 shadow-xl relative'>
-        <Image
-          src={`/${frontmatter.image}`}
-          alt={frontmatter.title}
-          layout='fill'
-          objectFit='cover'
-          className='p-2 rounded cursor-pointer'
-          objectPosition='top'
-        />
-      </div>
-      <h2 className='text-xl m-2 mt-5 text-indigo-600 font-semibold'>
-        {frontmatter.title}
-      </h2>
-      <p className='text-center font-normal mx-2 mb-5 flex-wrap'>
-        {frontmatter.description}
-      </p>
-      <Link href={`/projects/${slug}`}>
-        <a className='uppercase text-center font-normal font mb-5 flex-wrap'>
+    <Link href={`/projects/${slug}`}>
+      <a className='flex flex-col w-96 items-center justify-start hover:bg-gray-50 hover:shadow-xl rounded p-2'>
+        <div className='rounded w-80 h-80 shadow-xl relative'>
+          <Image
+            src={`/${frontmatter.image}`}
+            alt={frontmatter.title}
+            layout='fill'
+            objectFit='cover'
+            className='p-2 rounded cursor-pointer'
+            objectPosition='top'
+          />
+        </div>
+        <h2 className='text-xl m-2 mt-5 text-indigo-600 font-semibold'>
+          {frontmatter.title}
+        </h2>
+        <p className='text-center font-normal mx-2 mb-5 flex-wrap'>
+          {frontmatter.description}
+        </p>
+
+        <p className='uppercase text-center font-normal font mb-5 flex-wrap'>
           See Project Details &gt;
-        </a>
-      </Link>
-    </div>
+        </p>
+      </a>
+    </Link>
   );
 };
 
