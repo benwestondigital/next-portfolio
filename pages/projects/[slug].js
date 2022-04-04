029779f68fba52ca253d4edb7ffeaad9efa286ea
Ben <benwestondigital@gmail.com>
2022-04-04 12:40:39 +0100

    add next project link

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import Image from 'next/image';
 import md from 'markdown-it';
+import { useRouter } from 'next/router';
 import { DiGithubBadge } from 'react-icons/di';
 
 export async function getStaticPaths() {
@@ -19,22 +20,41 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
+  const files = fs.readdirSync('projects');
+  const paths = files.map(fileName => {
+    return fileName.replace('.md', '');
+  });
   const fileName = fs.readFileSync(`projects/${slug}.md`, 'utf-8');
   const { data: frontmatter, content } = matter(fileName);
   return {
     props: {
       frontmatter,
       content,
+      paths,
     },
   };
 }
 
-const ProjectPage = ({ frontmatter, content }) => {
+const ProjectPage = ({ frontmatter, content, paths }) => {
+  const router = useRouter();
+  const currentPath = router.query.slug;
+  const index = paths.findIndex(element => element === currentPath);
+  let nextProject;
+
+  if (index < paths.length -1) {
+    nextProject = paths[index + 1];
+  } else {
+    nextProject = paths[0];
+  }
+
   return (
     <div className='flex flex-col justify-between items-center md:mx-20 p-10 pt-20 md:pt-32 md:p-28'>
-        <h1 className='mb-5 font-bold text-3xl md:text-6xl'>
-          {frontmatter.title}
-        </h1>
+      <h1 className='mb-5 font-bold text-3xl md:text-6xl'>
+        {frontmatter.title}
+      </h1>
+      <Link href={`/projects/${nextProject}`}>
+        <a>Next Project {'>'}</a>
+      </Link>
       <div className='flex justify-between w-full'>
         <a href={frontmatter.github} target='_blank' rel='noreferrer'>
           Github Repo
