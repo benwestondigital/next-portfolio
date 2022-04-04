6e61e0764a43cd588e0ab9117f869691f3f8aa60
Ben <benwestondigital@gmail.com>
2022-04-04 14:15:08 +0100

    reduce margin for mobile

--- components/TechSection.jsx
+++ components/TechSection.jsx
@@ -1,7 +1,7 @@
 
 const TechSection = ({ techType, type }) => {
   return (
-    <div className='flex justify-items-start items-center mx-40 gap-5 mt-20'>
+    <div className='flex justify-items-start items-center mx-2 md:mx-40 gap-5 mt-20'>
       <div className='flex flex-col justify-center items-center'>
         <h2 className='text-xl text-indigo-600 font-semibold'>
           {techType}
