b9dad2933d3cd17fecf54a1ad67d04a6d0ae707c
Ben <benwestondigital@gmail.com>
2022-04-06 16:24:09 +0100

    remove double exclamation mark in hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       </div>
       <div className='flex flex-col mx-10 mt-10 md:w-1/2 md:ml-20'>
         <h1 className='mb-5 font-bold text-left text-5xl md:text-7xl'>
-          Hi! I&apos;m <span className='text-indigo-900'>Ben!</span> I love to
+          Hi! I&apos;m <span className='text-indigo-900'>Ben</span>. I love to
           create interesting projects with code.
         </h1>
       </div>
