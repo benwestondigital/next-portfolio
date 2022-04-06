d57fc0047c55e1b94baf367b6c9839e68139033b
Ben <benwestondigital@gmail.com>
2022-04-06 12:06:15 +0100

    remove unneeded classes add small mobile width

--- components/TechSection.jsx
+++ components/TechSection.jsx
@@ -1,18 +1,18 @@
 
 const TechSection = ({ techType, type }) => {
   return (
-    <section className='flex justify-items-start items-center mx-2 md:mx-40 gap-5 mt-20'>
+    <section className='my-10'>
       <div className='flex flex-col justify-center items-center'>
         <h2 className='text-xl text-indigo-600 font-semibold'>
           {techType}
         </h2>
-        <div className='flex flex-wrap shrink justify-center items-start'>
+        <div className='flex flex-1 flex-wrap shrink justify-center items-start'>
           {type[techType].map(item => {
             const Icon = item.icon;
             return (
               <div
                 key={item.techName}
-                className='flex flex-col md:w-20 justify-start items-center mx-2'
+                className='flex flex-col w-20 justify-start items-center mx-2'
               >
                 <Icon
                   className='w-10 h-10 bg-gray-600 hover:bg-gray-800 text-gray-100 m-3 p-1 shadow-xl rounded-md'
