efae45cb67f3d183c37abb2da97660a38184bdf9
Ben <benwestondigital@gmail.com>
2022-04-06 12:05:32 +0100

    reduce h2 small mobile remove unnecessary classes

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,11 +5,11 @@ const Projects = ({projects}) => {
   return (
     <Element id='projects' name='projects'>
       <section className='flex flex-col items-center justify-center w-full h-auto my-32'>
-        <h2 className='text-6xl font-bold text-indigo-900'>Projects</h2>
-        <p className='mt-5 mb-5 font-normal text-center'>
+        <h2 className='text-5xl sm:text-6xl font-bold text-indigo-900'>Projects</h2>
+        <p className='my-5 text-center'>
           Here are some of the projects I&apos;ve been working on lately:
         </p>
-        <div className='items-center justify-center gap-10 mt-10 md:flex flex-wrap sm:flex-col md:flex-row md:items-start md:gap-10 sm:pb-10'>
+        <div className='items-center justify-center gap-6 md:gap-10 mt-10 flex flex-wrap flex-col md:flex-row'>
           {projects.map(project => {
             return <SingleProject key={project.slug} project={project} />;
           })}
