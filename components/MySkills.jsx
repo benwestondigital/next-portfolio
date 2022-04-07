40c4d58692021d95555a66a8185e19a3ab7dd506
Ben <benwestondigital@gmail.com>
2022-04-07 17:44:22 +0100

    add prose remove a underlining

--- components/MySkills.jsx
+++ components/MySkills.jsx
@@ -9,11 +9,11 @@ const Tech = () => {
         <h1 className='text-5xl sm:text-6xl font-bold text-indigo-900'>
           My Skills
         </h1>
-        <p className='flex-wrap md:w-2/3 lg:w-1/2 my-5 text-center'>
+        <p className='flex-wrap md:w-2/3 lg:w-1/2 my-5 text-center prose'>
           I am always looking to learn new technology, so in addition to my
           bootcamp I have completed a number of online courses such as{' '}
           <a
-            className='font-semibold'
+            className='font-semibold no-underline'
             href='https://www.codecademy.com/learn/learn-html'
             target='_blank'
             rel='noreferrer'
@@ -22,7 +22,7 @@ const Tech = () => {
           </a>
           ,{' '}
           <a
-            className='font-semibold'
+            className='font-semibold no-underline'
             href='https://www.codecademy.com/learn/introduction-to-javascript'
             target='_blank'
             rel='noreferrer'
@@ -31,7 +31,7 @@ const Tech = () => {
           </a>{' '}
           and{' '}
           <a
-            className='font-semibold'
+            className='font-semibold no-underline'
             href='https://www.codecademy.com/learn/learn-typescript'
             target='_blank'
             rel='noreferrer'
