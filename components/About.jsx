4f9c5a1b33da77c816701077d5656e9c43588002
Ben <benwestondigital@gmail.com>
2022-04-07 20:11:28 +0100

    remove underline from a and remove unneeded class

--- components/About.jsx
+++ components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
         >
           See My Portfolio
         </Link>
-        <div className='prose lg:prose-xl flex-wrap mb-5 font-normal text-left px-2'>
+        <div className='prose lg:prose-xl flex-wrap mb-5 px-2'>
           <p className='pt-16'>
             I first became interested in coding through playing online games
             when I was a teenager. I saw people creating programs to automate
@@ -54,7 +54,7 @@ const About = () => {
               href='https://northcoders.com/'
               target='_blank'
               rel='noreferrer'
-              className='font-semibold'
+              className='font-semibold no-underline'
             >
               Northcoders
             </a>{' '}
