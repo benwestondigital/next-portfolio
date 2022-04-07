c8efcf50c32cfe5504ed0abf28571e2163f75a71
Ben <benwestondigital@gmail.com>
2022-04-07 17:44:06 +0100

    add flex-1 to divs and center items

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,8 +3,8 @@ import avatar from '../public/images/avatar.png';
 
 const Hero = () => {
   return (
-    <section className='flex justify-center md:mx-20 md:pt-32 pt-28'>
-      <div className='relative hidden w-1/3 px-10 rounded-full shadow-xl h-96 md:block'>
+    <section className='flex justify-center items-center md:mx-20 md:pt-32 pt-28'>
+      <div className='relative flex-1 hidden w-1/3 rounded-full shadow-xl h-96 md:block'>
         <Image
           src={avatar}
           alt='hero'
@@ -14,7 +14,7 @@ const Hero = () => {
           className='hidden rounded-full cursor-pointer md:block'
         />
       </div>
-      <div className='flex flex-col mx-10 mt-10 md:w-1/2 md:ml-20'>
+      <div className='flex flex-1 flex-col mx-10 mt-10 md:w-3/5 lg:w-1/2 md:ml-20'>
         <h1 className='mb-5 font-bold text-left text-5xl md:text-6xl'>
           Hi! I&apos;m <span className='text-indigo-900'>Ben</span>. I love to
           create interesting projects with code.
