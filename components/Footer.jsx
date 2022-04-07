66ead944dee542d2e89ecbe8eef45b6b0427876f
Ben <benwestondigital@gmail.com>
2022-04-07 17:43:43 +0100

    edit styling add built with section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,17 @@
 const Footer = () => {
   return (
-    <div className='h-16 bg-gray-50 flex flex-col justify-center items-center'>
-      <h3 className='text-xl text-blue-900 mr-2'>
-        Made by{' '}
+    <div className='mt-16 flex flex-col justify-center items-center'>
+      <h3 className='text-xl'>
         <a
           href='#'
-          className='cursor-pointer font-semibold text-indigo-900 hover:text-black'
+          className='cursor-pointer font-semibold hover:text-black'
         >
-          Ben Weston
+          Ben <span className='text-blue-600'>Weston</span>
         </a>
       </h3>
-      <p className="text-sm text-gray-600">Built with Next.js, Tailwind and MDX</p>
+      <p className='text-sm pt-2 pb-4 text-gray-600'>
+        Built with Next.js, Tailwind and MDX
+      </p>
     </div>
   );
 };
