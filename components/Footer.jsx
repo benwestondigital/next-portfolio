178726c507331be1bb64386edab1bee13e8a59e3
Ben <benwestondigital@gmail.com>
2022-04-07 20:11:36 +0100

    fix link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,14 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <div className='mt-16 flex flex-col justify-center items-center'>
       <h3 className='text-xl'>
-        <a
-          href='#'
-          className='cursor-pointer font-semibold hover:text-black'
-        >
-          Ben <span className='text-blue-600'>Weston</span>
-        </a>
+        <Link href='/'>
+          <a className='cursor-pointer font-semibold hover:text-black'>
+            Ben <span className='text-blue-600'>Weston</span>
+          </a>
+        </Link>
       </h3>
       <p className='text-sm pt-2 pb-4 text-gray-600'>
         Built with Next.js, Tailwind and MDX
