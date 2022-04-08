f296773a9a7aa8d422d291b1f2711080f2f78c10
Ben <benwestondigital@gmail.com>
2022-04-08 13:10:28 +0100

    improve slideIn animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,14 +19,16 @@ module.exports = {
           '100%': { opacity: 1, transform: 'translateY(0%)' },
         },
         translateRight: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          '0%': { opacity: 0, transform: 'translateX(-100%)' },
+          '90%': {opacity: 1, transform: 'translateX(12%)'},
+          '100%': { opacity: 1, transform: 'translateX(0%)' },
         },
       },
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
+        wiggleShort: 'wiggle 0.3s ease-in-out',
         fadeIn: 'fadeIn 1s ease-in forwards',
-        slideIn: 'translateRight 0.4s ease forwards',
+        slideIn: 'translateRight 0.8s ease forwards',
       },
     },
     fontFamily: {
