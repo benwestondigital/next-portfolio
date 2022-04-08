e03f1b1366413e1e4c1c4e01ac5be7094d10e308
Ben <benwestondigital@gmail.com>
2022-04-08 13:10:15 +0100

    add animation on button click

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import avatar from '../public/images/avatar.png';
 
 const Hero = () => {
+  const [effect, setEffect] = useState(false);
   return (
     <section className='flex items-center justify-center pt-28 md:mx-20 md:pt-32'>
       <div className='relative hidden h-96 w-1/3 flex-1 rounded-full shadow-xl md:block'>
@@ -11,7 +13,11 @@ const Hero = () => {
           layout='fill'
           objectFit='contain'
           priority
-          className='hidden cursor-pointer rounded-full md:block'
+          className={`${
+            effect && 'animate-wiggleShort'
+          } hidden cursor-pointer rounded-full md:block`}
+          onClick={() => setEffect(true)}
+          onAnimationEnd={() => setEffect(false)}
         />
       </div>
       <div className='mx-10 mt-10 flex flex-1 flex-col md:ml-20 md:w-3/5 lg:w-1/2'>
