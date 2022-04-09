fa5635cae2c17dd62e5efa03e5fa83e20e09beac
Ben <benwestondigital@gmail.com>
2022-04-09 15:21:00 +0100

    add darkmode button to mobile menu

--- components/MobileLinks.jsx
+++ components/MobileLinks.jsx
@@ -2,6 +2,7 @@ import { Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { Link as ScrollLink } from 'react-scroll';
 import { navLinks } from '../utils';
+import RenderThemeChanger from './RenderThemeChanger';
 
 const MobileLinks = ({ path, isOpen, setIsOpen }) => {
   return (
@@ -15,7 +16,7 @@ const MobileLinks = ({ path, isOpen, setIsOpen }) => {
       leaveTo='opacity-0 scale-95 -translate-x-full'
     >
       <div className='md:hidden' id='mobile-menu'>
-        <div className='w-full space-y-1 bg-white dark:bg-gray-900 py-4'>
+        <div className='w-full space-y-1 bg-white py-4 dark:bg-gray-900'>
           {navLinks.map(link => {
             return (
               <div key={link.path}>
@@ -27,7 +28,7 @@ const MobileLinks = ({ path, isOpen, setIsOpen }) => {
                     smooth={true}
                     offset={-100}
                     duration={500}
-                    className='block font-semibold cursor-pointer rounded-md px-3 py-2 text-black dark:text-white hover:bg-blue-600 hover:text-white'
+                    className='block cursor-pointer rounded-md px-3 py-2 font-semibold text-black hover:bg-blue-600 hover:text-white dark:text-white'
                     onClick={() => setIsOpen(false)}
                   >
                     {link.name}
@@ -36,7 +37,7 @@ const MobileLinks = ({ path, isOpen, setIsOpen }) => {
                   <Link href={`/#${link.path}`}>
                     <a
                       onClick={() => setIsOpen(false)}
-                      className='block cursor-pointer rounded-md px-3 py-2 font-semibold text-black dark:text-white hover:bg-blue-600 hover:text-white'
+                      className='block cursor-pointer rounded-md px-3 py-2 font-semibold text-black hover:bg-blue-600 hover:text-white dark:text-white'
                     >
                       {link.name}
                     </a>
@@ -45,6 +46,9 @@ const MobileLinks = ({ path, isOpen, setIsOpen }) => {
               </div>
             );
           })}
+          <div className='pl-3 pt-8'>
+            <RenderThemeChanger />
+          </div>
         </div>
       </div>
     </Transition>
