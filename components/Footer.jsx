8b188eab6b3bb71bef9f035b2dfa87555a397267
Ben <benwestondigital@gmail.com>
2022-04-09 13:32:02 +0100

    add dark mode styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,23 +6,27 @@ const Footer = () => {
       <h3 className='pt-4 text-xl'>
         <Link href='/'>
           <a className='cursor-pointer font-semibold hover:opacity-60'>
-            <span className='hover:text-black dark:hover:text-gray-500'>Ben</span>{' '}
-            <span className='text-blue-600 hover:text-indigo-900 dark:hover:text-indigo-500'>Weston</span>
+            <span className='hover:text-black dark:hover:text-gray-500'>
+              Ben
+            </span>{' '}
+            <span className='text-blue-600 hover:text-indigo-900 dark:hover:text-indigo-500'>
+              Weston
+            </span>
           </a>
         </Link>
       </h3>
       <p className='prose pt-2 pb-4 text-sm dark:prose-invert'>
         Built with Next.js, Tailwind and MDX
       </p>
-      <div className='prose dark:prose-dark mt-4 mr-8 flex flex-col self-end text-sm md:hidden'>
-        <p className='mb-1 text-xs uppercase tracking-wide text-gray-600'>
+      <div className='dark:prose-dark prose mt-4 mr-8 flex flex-col self-end text-sm md:hidden'>
+        <p className='mb-1 text-xs uppercase tracking-wide text-gray-600 dark:text-gray-400'>
           Links
         </p>
         <a
           href='https://www.linkedin.com/in/ben-weston-b19420175/'
           target='_blank'
           rel='noreferrer'
-          className='pb-1 font-semibold text-black no-underline hover:animate-wiggle hover:text-orange-400 active:underline'
+          className='pb-1 font-semibold text-black no-underline hover:animate-wiggle hover:text-orange-400 active:underline dark:text-white dark:hover:text-orange-400'
         >
           LinkedIn
         </a>
@@ -30,7 +34,7 @@ const Footer = () => {
           href='https://github.com/benwestondigital/'
           target='_blank'
           rel='noreferrer'
-          className='py-1 font-semibold text-black no-underline hover:animate-wiggle hover:text-orange-400 active:underline'
+          className='py-1 font-semibold text-black no-underline hover:animate-wiggle hover:text-orange-400 active:underline dark:text-white dark:hover:text-orange-400'
         >
           GitHub
         </a>
@@ -38,7 +42,7 @@ const Footer = () => {
           href='https://twitter.com/benwestononline'
           target='_blank'
           rel='noreferrer'
-          className='py-1 pb-4 font-semibold text-black no-underline hover:animate-wiggle hover:text-orange-400 active:underline'
+          className='py-1 pb-4 font-semibold text-black no-underline hover:animate-wiggle hover:text-orange-400 active:underline dark:text-white dark:hover:text-orange-400'
         >
           Twitter
         </a>
