6ebd75e1df9fa711426bfc1b53ee57396aabb77c
Ben <benwestondigital@gmail.com>
2022-04-09 20:39:55 +0100

    add px-2 to improve small screens

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,15 +1,17 @@
 import { Element } from 'react-scroll';
 import SingleProject from './SingleProject';
 
-const Projects = ({projects}) => {
+const Projects = ({ projects }) => {
   return (
     <Element id='projects' name='projects' className='scroll-mt-24'>
-      <section className='flex flex-col items-center justify-center w-full h-auto my-32'>
-        <h2 className='text-5xl sm:text-6xl font-bold text-indigo-900 dark:text-indigo-500'>Projects</h2>
-        <p className='my-5 text-center prose lg:prose-xl dark:prose-invert'>
+      <section className='my-32 flex flex-col items-center justify-center px-2'>
+        <h2 className='text-5xl font-bold text-indigo-900 dark:text-indigo-500 sm:text-6xl'>
+          Projects
+        </h2>
+        <p className='prose my-5 text-center dark:prose-invert lg:prose-xl'>
           Here are some of the projects I&apos;ve been working on lately:
         </p>
-        <div className='items-center justify-center gap-6 md:gap-10 px-2 mt-10 flex flex-wrap flex-col md:flex-row'>
+        <div className='mt-10 flex flex-col flex-wrap items-center justify-center gap-6 px-2 md:flex-row md:gap-10'>
           {projects.map(project => {
             return <SingleProject key={project.slug} project={project} />;
           })}
