a1a76762a7188079e32b324f714b1d81f5580863
Ben <benwestondigital@gmail.com>
2022-04-09 17:07:10 +0100

    remove old code related to next project link

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,10 +1,8 @@
-import Link from 'next/link';
 import fs from 'fs';
 import matter from 'gray-matter';
 import Image from 'next/image';
 import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemote } from 'next-mdx-remote';
-import { useRouter } from 'next/router';
 import { Element } from 'react-scroll';
 import Head from 'next/head';
 import { FaExternalLinkAlt } from 'react-icons/fa';
@@ -14,32 +12,13 @@ import { AiFillGithub } from 'react-icons/ai';
 //TODO: add 4 pictures for each project - screenshots size 359*432
 //TODO: for code, I can add snippets rather than having to do a screenshot
 
-const ProjectPage = ({ frontmatter, mdxSource, paths }) => {
-  const router = useRouter();
-  const currentPath = router.query.slug;
-  const index = paths.findIndex(element => element === currentPath);
-  let nextProject;
-
-  if (index < paths.length - 1) {
-    nextProject = paths[index + 1];
-  } else {
-    nextProject = paths[0];
-  }
-
+const ProjectPage = ({ frontmatter, mdxSource }) => {
   return (
     <Element id='projectpage' name='projectpage'>
       <Head>
         <title>Ben Weston | {frontmatter.title}</title>
       </Head>
       <article className='flex flex-col items-center justify-between px-4 pt-20 md:pt-28'>
-        {/*         <div className='flex justify-between w-4/5 sm:w-2/3 my-4 text-lg text-gray-700'>
-          <Link href='/'>
-            <a className='cursor-pointer hover:font-semibold'>Home</a>
-          </Link>
-          <Link href={`/projects/${nextProject}`}>
-            <a className='hover:font-semibold'>Next Project</a>
-          </Link>
-        </div> */}
         <div className='mb-4 mt-24 flex h-32 flex-col justify-center md:items-center'>
           <h2 className='text-lg font-semibold text-blue-600'>
             {frontmatter.type}
@@ -78,7 +57,7 @@ const ProjectPage = ({ frontmatter, mdxSource, paths }) => {
             className='rounded'
           />
         </div>
-        <div className='prose dark:prose-invert pt-6'>
+        <div className='prose pt-6 dark:prose-invert'>
           <MDXRemote {...mdxSource} components={components} />
         </div>
       </article>
@@ -116,10 +95,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const files = fs.readdirSync('projects');
-  const paths = files.map(fileName => {
-    return fileName.replace('.mdx', '');
-  });
   const fileName = fs.readFileSync(`projects/${slug}.mdx`, 'utf-8');
   const { data: frontmatter, content } = matter(fileName);
   const mdxSource = await serialize(content);
@@ -128,7 +103,6 @@ export async function getStaticProps({ params: { slug } }) {
     props: {
       frontmatter,
       mdxSource,
-      paths,
     },
   };
 }
