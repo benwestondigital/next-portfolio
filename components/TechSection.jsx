932440c495c062c231085f9ca07b46501e369237
Ben <benwestondigital@gmail.com>
2022-04-09 16:36:18 +0100

    remove sm tag for text

--- components/TechSection.jsx
+++ components/TechSection.jsx
@@ -12,7 +12,7 @@ const TechSection = ({ techType, type }) => {
                 className='mx-2 flex w-20 flex-col items-center justify-start'
               >
                 <Icon className='m-3 h-10 w-10 rounded-md bg-gray-600 p-1 text-gray-100 shadow-xl hover:bg-gray-800' />
-                <p className='text-center text-gray-400 sm:text-sm md:text-base'>
+                <p className='text-center text-gray-400 text-sm md:text-base'>
                   {item.techName}
                 </p>
               </div>
