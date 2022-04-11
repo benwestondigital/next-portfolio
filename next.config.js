b28afce8249f716165a7fda2cda9935b07506347
Ben <benwestondigital@gmail.com>
2022-04-11 09:56:17 +0100

    fix jsx runtime not exported error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // fixes next-mdx-remote: Package path ./jsx-runtime.js is not exported from package react
+      // https://github.com/hashicorp/next-mdx-remote/issues/237
+      'react/jsx-runtime.js': require.resolve('react/jsx-runtime'),
+    };
+    return config;
+  },
+  /* In node_modules/next-mdx-remote/dist/index.js I changed:
+     "import * as runtime from 'react/jsx-runtime.js';" to
+     "import * as runtime from 'react/jsx-runtime';"
+     and added the above webpack section which has resolved the error.
+     solution found at: https://github.com/hashicorp/next-mdx-remote/issues/237
+     */
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
